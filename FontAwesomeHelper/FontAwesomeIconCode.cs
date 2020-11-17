76788223618b412ffd5b38e24845a076d79eda1f
Jakub Ociepa <jakub.ociepa@outlook.com>
2020-11-17 19:13:00 +0100

    Add FontAwesomeIconCode class with codes for icons that the name starts with 'a'

---
+++ FontAwesomeHelper/FontAwesomeIconCode.cs
@@ -0,0 +1,53 @@
+﻿namespace FontAwesomeHelper
+{
+    public static partial class FontAwesomeIconCode
+    {
+        public static class Solid
+        {
+            public const string Ad = "\uf641";
+            public const string AddressBook = "\uf2b9";
+            public const string AddressCard = "\uf2bb";
+            public const string Adjust = "\uf042";
+            public const string AirFreshener = "\uf5d0";
+            public const string AlignCenter = "\uf037";
+            public const string AlignJustify = "\uf039";
+            public const string AlignLeft = "\uf036";
+            public const string AlignRight = "\uf038";
+            public const string Allergies = "\uf461";
+            public const string Ambulance = "\uf0f9";
+            public const string AmericanSignLanguageInterpreting = "\uf2a3";
+            public const string Anchor = "\uf13d";
+            public const string AngleDoubleDown = "\uf103";
+            public const string AngleDoubleLeft = "\uf100";
+            public const string AngleDoubleRight = "\uf101";
+            public const string AngleDoubleUp = "\uf102";
+            public const string Angry = "\uf556";
+            public const string Ankh = "\uf644";
+            public const string AppleAlt = "\uf5d1";
+            public const string Archive = "\uf187";
+            public const string Archway = "\uf557";
+            public const string ArrowAltCircleDown = "\uf358";
+            public const string ArrowAltCircleLeft = "\uf359";
+            public const string ArrowAltCircleRigth = "\uf35a";
+            public const string ArrowAltCircleUp = "\uf35b";
+            public const string ArrowCircleDown = "\uf0ab";
+            public const string ArrowCircleLeft = "\uf0a8";
+            public const string ArrowCircleRight = "\uf0a9";
+            public const string ArrowCircleUp = "\uf0aa";
+            public const string ArrowDown = "\uf063";
+            public const string ArrowLeft = "\uf060";
+            public const string ArrowRight = "\uf061";
+            public const string ArrowUp = "\uf062";
+            public const string ArrowsAlt = "\uf0b2";
+            public const string ArrowsAltH = "\uf337";
+            public const string ArrowsAltV = "\uf338";
+            public const string AssistiveListeningSystem = "\uf2a2";
+            public const string Asterisk = "\uf069";
+            public const string At = "\uf1fa";
+            public const string Atlas = "\uf558";
+            public const string Atom = "\uf5d2";
+            public const string AudioDescription = "\uf29e";
+            public const string Award = "\uf559";
+        }
+    }
+}
